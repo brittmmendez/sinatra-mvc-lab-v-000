c8bc290332d2bfb6268648dd579b6e4fc9d3ad0d
Brittany Morris <brittmmendez@gmail.com>
2018-01-22 13:02:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
+  get '/' do
     erb :user_input
   end
 
   post '/' do
     @user_input=PigLatinize.new(params[:text])
 
-    erb :results    
+    erb :results
   end
 end
