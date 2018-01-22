89641d2597c39b2192cb16f4f44978505dd7562d
Brittany Morris <brittmmendez@gmail.com>
2018-01-22 13:46:44 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinize
+  attr_reader :text
+
+  def initialize(text)
+    @text=text
+  end
+
+  def split
+    @text.split(" ").collect do |word|
+      if word.start_with?('a','e','i','o','u')  
+        "#{word}ay"
+      else 
+        word.split("")
+        "#{word[1..-1]}#{word[0]}ay"
+      end
+    end
+  end
+
+end
